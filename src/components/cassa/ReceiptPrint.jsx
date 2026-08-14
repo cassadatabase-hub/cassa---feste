3ef3eebac15c185d5285c9587e9f124b7237f832
cassadatabase-hub <cassadatabase@gmail.com>
2026-08-14 02:47:23 +0200

    Update ReceiptPrint.jsx

--- src/components/cassa/ReceiptPrint.jsx
+++ src/components/cassa/ReceiptPrint.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 import { formatTime, formatDate, formatPrice } from '@/lib/codeGen';
 
 export default function ReceiptPrint({ order, festaName, productOptions = [] }) {
